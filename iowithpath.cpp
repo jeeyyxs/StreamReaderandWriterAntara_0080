48e3724446d4053d0db6fb80197849de4038fc71
jeeyyxs <riodwimulyono0512@gmail.com>
2026-08-18 10:45:11 +0700

    menambahkan contoh file

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -7,11 +7,16 @@ int main() {
     //deklarasi variabel baris dan namafile
     string baris, NamaFile;
 
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
     //membuat file dalam mode menulis
     ofstream outfile;
 
-    //menunjuk ke sebuah nama file dengan ekstensi txt
-    outfile.open("ContohFile.txt");
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open("C:\\Users\\LENOVO\\Documents\\contoh\\" + NamaFile +
+    ".txt", ios :: out);
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
