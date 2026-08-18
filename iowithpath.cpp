d0405bef24235b1c74d05712c37346fce76b73f0
jeeyyxs <riodwimulyono0512@gmail.com>
2026-08-18 10:19:20 +0700

    membuat file dalam mode menulis dan menunjuk ke sebuah nama file dengan ekstensi txt

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int main() {
     //deklarasi variabel baris dan namafile
-    string baris, Namafile;
+    string baris, NamaFile;
 
     //membuat input untuk menangkap nama file
     cout << "Masukkan nama file: ";
-    cin >> Namafile;
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open("C:\\Users\\LENOVO\\Documents\\contoh\\" + NamaFile +
+    ".txt", ios :: out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
