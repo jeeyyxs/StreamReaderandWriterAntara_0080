c3df1c5dfa94398a547f0c963d686e85ebcec390
jeeyyxs <riodwimulyono0512@gmail.com>
2026-08-18 10:25:24 +0700

    kondisi jika file ada

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -38,4 +38,18 @@ int main() {
 
     //menunjuk ke sebuah file dengan ekstensi txt
     infile.open(NamaFile + ".txt", ios :: in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+
+    //kondisi jika file ada
+    if (infile.is_open()) {
+        //looping untuk membaca isi file
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        //menutup file apabila telah selesai dibaca
+        infile.close();
+    }
+
+    
 }
